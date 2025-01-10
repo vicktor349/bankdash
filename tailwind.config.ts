8143e3f9daccacaa2a7ce847e21efe9730384e69
vicktor349 <ayomidevictor349@gmail.com>
2025-01-10 10:40:11 +0100

    feature: More breakpoint for responsivity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,18 @@ export default {
       colors: {
         "primary": "#2D60FF",
         "backgroundColor": "#E5E5E5",
-        "logoColor": "var(--Primary-2, #343C6A)"
+        "logoColor": "var(--Primary-2, #343C6A)",
+        "inputColor": "#8BA3CB"
       },
+      backgroundColor: {
+        "inputBackground": "#F5F7FA"
+      },
+      screens: {
+        "3xl": "1736px",
+        "ssm": "540px",
+        "sssm": "480px",
+        "ssssm": "360px"
+      }
     },
   },
   plugins: [],
